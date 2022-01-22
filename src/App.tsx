f4d000cd8d6a7193e7d1e2f8b0fc76666649d16a
Badini Rachid Rodrigue <brodrigue@protonmail.com>
2022-01-22 09:33:18 +0000

    create AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 interface Props {
 
 }
 
+const ToggleButton = () => {
+  return (
+    <IconButton
+      size="large"
+      edge="start"
+      color="inherit"
+      aria-label="menu"
+    >
+      <MenuIcon />
+    </IconButton>
+  );
+};
+
 const App: React.FC<Props> = () => {
   return (
-    <div>
-    </div>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <ToggleButton />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+
+      </AppBar>
+
+
+    </Box>
   );
 }
 
